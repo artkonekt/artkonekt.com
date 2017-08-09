2e1ced9489fb983aa7d2d4b56a68d55951771bd0
Attila Fulop <fulopattila122@gmail.com>
2017-08-09 17:40:13 +0300

    Cleanup + sitemaps - Sitemap sculpin extension has beenadded, - all pages are now included in sitemap - composer packages updated - removed surplus code (typekit font, jquery)

---
+++ app/SculpinKernel.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Contains the Sculpin Kernel class.
+ *
+ * @copyright   Copyright (c) 2017 Attila Fulop
+ * @author      Attila Fulop
+ * @license     MIT
+ * @since       2017-08-09
+ *
+ */
+
+
+use Nickpeirson\Sculpin\Bundle\SitemapBundle\SculpinSitemapBundle;
+use Sculpin\Bundle\SculpinBundle\HttpKernel\AbstractKernel;
+
+class SculpinKernel extends AbstractKernel
+{
+    protected function getAdditionalSculpinBundles()
+    {
+        return [
+            SculpinSitemapBundle::class
+        ];
+    }
+}
